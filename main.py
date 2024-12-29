5648febde4dab691a8399d4bd7f2f79966c88e44
Collin Farell <Heavender07@gmail.com>
2024-12-29 20:40:47 +0300

    This file is the main executable file

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from src.masks import get_mask_card_number
 from src.masks import get_mask_account
-from src.masks import number_of_bill
+from src.widget import mask_account_card
 
 hidden_card_number = get_mask_card_number('5678904547123457')
 print(hidden_card_number)
 
-hidden_bill_number = get_mask_account('5678904547123455')
+hidden_bill_number = get_mask_account('5678904547123457')
 print(hidden_bill_number)
+
+types_cards = mask_account_card('Visa Platinum')
+print(types_cards)
+
+
