11aa36c68f579e92495a91eb4171663789b7e280
Collin Farell <Heavender07@gmail.com>
2025-01-08 11:25:36 +0300

    revision of task 10.1

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ processing_check_canceled_filter = filter_by_state(
     [
         {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
     ]
 )
 print(processing_check_canceled_filter)
--- src/processing.py
+++ src/processing.py
@@ -3,31 +3,33 @@ from typing import Dict, List
 # импортируем библиотеку для аннотации функции
 
 
-def filter_by_state(l_lict: List[Dict], key: str = "EXECUTED") -> List[Dict]:
-    # Создаем функцию фильтрации
+def filter_by_state(l_lict: List[Dict], key_s: str = "EXECUTED") -> List[Dict]:
+    """
+    принимает список словарей и опционально значение для ключа state(по умолчанию 'EXECUTED').
+    Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
+    соответствует указанному значению.
+    """
+    # Создаем логиику функции фильтрации
     new_filter_by_state = []
+    # Создаем цикл для перебора данных
     for item in l_lict:
-        if item["state"] == key:
-            new_filter_by_state.append(item)
-        else:
+        # Создаем условие для фильтрации
+        if item["state"] == key_s:
+            # Добавляем данные в список
             new_filter_by_state.append(item)
     return new_filter_by_state
 
 
-def sort_by_date(last_dict: List[Dict], keys_date: str = "True") -> List[Dict]:
-    # Создаем функцию сортировки по дате
+def sort_by_date(last_dict: List[Dict], descending: bool = "True") -> List[Dict]:
+    """
+    Принимает список словарей и необязательный параметр, задающий порядок сортировки
+    (по умолчанию — убывание). Функция должна возвращать новый список, отсортированный по дате (date
+    ).
+    """
     # Создаем пустой список для неотсортированных данных
     new_date_list = []
-    # Создаем пустой список для сортировки
-    sorted_date_list = []
     for item in last_dict:
         # В цикле перебираем данные и добавляем в список
         new_date_list.append({"id": item["id"], "state": item["state"], "date": item["date"]})
-    # Сортируем данные по ключу date и добавляем в список согласно сортировке
-    if keys_date == "True":
-        sorted_date_list = sorted(new_date_list, key=lambda x: x["date"], reverse=True)
-    else:
-        sorted_date_list = sorted(new_date_list, key=lambda x: x["date"])
 
-    return sorted_date_list
-
+    return sorted(last_dict, key=lambda x: x["date"], reverse=descending)
